dcaa0e60e56cdcc2d20f7fba0ad23098dcb7606a
Mauro De Mey <mauro.demey@student.ap.be>
2025-04-22 15:53:05 +0200

    some changes

---
+++ Project/Assets/Scripts/Jumper.cs
@@ -0,0 +1,54 @@
+using System;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class Jumper : Agent
+{
+    [SerializeField]
+    float _jumpForce = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        Initialize();
+    }
+    public override void Initialize()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        resetPosition();
+    }
+
+    private void resetPosition()
+    {
+        this.transform.localPosition = new Vector3(-9, 1f, 0);
+        rb.linearVelocity = Vector3.zero;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+       
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        Debug.Log("ActionReceived");
+        if (Mathf.FloorToInt(actions.DiscreteActions[0]) == 1 && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        {
+            Console.WriteLine("Jumping");
+            rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        Debug.Log("Heuristic method called");
+        var discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+        Debug.Log($"Discrete action set to: {discreteActions[0]}");
+    }
+}
